637012af95485e1d10d3e77d7adfea52e1160a6f
Ubuntu <ubuntu@ip-172-31-36-68.ap-south-1.compute.internal>
2026-02-22 07:00:13 +0000

    this is py file

---
+++ test.py
@@ -0,0 +1,15 @@
+# Simple Python test program
+
+def main():
+    print("Hello, World!")
+    print("This is a basic Python test program.")
+    
+    # Simple calculation
+    a = 5
+    b = 3
+    sum_result = a + b
+    
+    print("The sum of", a, "and", b, "is:", sum_result)
+
+if __name__ == "__main__":
+    main()
